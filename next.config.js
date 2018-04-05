ef0c52546420a191c0b92695b3df3ef7854c9a50
kevin schuchard <schuchard.kevin@gmail.com>
2018-04-04 21:43:22 -0600

    add next static app

---
+++ next.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  exportPathMap: function(defaultPathMap) {
+    const pageIDs= Array.from(new Array(3), (x, i) => i);
+    // transform the list of posts into a map of pages with the pathname `/post/:id`
+    const pages = pageIDs.reduce(
+      (pages, post) =>
+        Object.assign({}, pages, {
+          [`/about/${post}`]: {
+            page: '/about',
+            query: { id: post },
+          },
+        }),
+      {}
+    );
+
+    // combine the map of post pages with the home
+    return {
+      ...pages,
+      ...{ '/': { page: '/' } },
+    };
+  },
+};
