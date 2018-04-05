5727fa0d7117c76d0a23e3874d3eca4b8bdf127f
kevin schuchard <schuchard.kevin@gmail.com>
2018-04-04 23:23:48 -0600

    spread op

--- next.config.js
+++ next.config.js
@@ -4,16 +4,17 @@ module.exports = {
   postIDS,
   exportPathMap: (defaultPathMap) => {
     // transform the list of posts into a map of pages with the pathname `/post/:id`
-    const generatedPosts = postIDS.reduce(
-      (pages, post) =>
-        Object.assign({}, pages, {
+    const generatedPosts = postIDS.reduce((pages, post) => {
+      return {
+        ...pages,
+        ...{
           [`/post/${post}`]: {
             page: '/post',
             query: { id: post },
           },
-        }),
-      {}
-    );
+        },
+      };
+    }, {});
 
     // combine the map of post pages with the home
     return {
