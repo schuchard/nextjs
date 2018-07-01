25465d53e898b92445c2767f9acee1226a3e1923
Kevin Schuchard <schuchard.kevin@gmail.com>
2018-06-30 22:03:19 -0600

    add example text, ignore map files

--- pages/post.js
+++ pages/post.js
@@ -10,7 +10,37 @@ export default class extends Component {
     return (
       <main>
         <h1>Post {this.props.id}</h1>
-        <Link href='/'>
+        <span>
+          Ut suscipit vero voluptatem molestiae. Veritatis itaque numquam perspiciatis. Deleniti
+          autem nobis iste ut eaque et totam ipsum ut. Odio fugiat nihil consectetur eius provident.
+          Cupiditate dolore facilis omnis et vel quia deleniti et. Rem ea ullam optio est quam.
+          Beatae architecto consequatur laudantium et aut consequatur est eum quis. Dolore
+          voluptatem amet qui tenetur quidem. Sunt veniam maxime architecto animi sequi libero quia
+          architecto. Porro officia sequi quo. Qui commodi qui corporis molestias totam qui. Ipsa
+          cum repellat. Est suscipit tempora non. Maiores excepturi rerum iure qui impedit. Ut
+          suscipit vero voluptatem molestiae. Veritatis itaque numquam perspiciatis. Deleniti autem
+          nobis iste ut eaque et totam ipsum ut. Odio fugiat nihil consectetur eius provident.
+          Cupiditate dolore facilis omnis et vel quia deleniti et. Rem ea ullam optio est quam.
+          Beatae architecto consequatur laudantium et aut consequatur est eum quis. Dolore
+          voluptatem amet qui tenetur quidem. Sunt veniam maxime architecto animi sequi libero quia
+          architecto. Porro officia sequi quo. Qui commodi qui corporis molestias totam qui. Ipsa
+          cum repellat. Est suscipit tempora non. Maiores excepturi rerum iure qui impedit. Ut
+          suscipit vero voluptatem molestiae. Veritatis itaque numquam perspiciatis. Deleniti autem
+          nobis iste ut eaque et totam ipsum ut. Odio fugiat nihil consectetur eius provident.
+          Cupiditate dolore facilis omnis et vel quia deleniti et. Rem ea ullam optio est quam.
+          Beatae architecto consequatur laudantium et aut consequatur est eum quis. Dolore
+          voluptatem amet qui tenetur quidem. Sunt veniam maxime architecto animi sequi libero quia
+          architecto. Porro officia sequi quo. Qui commodi qui corporis molestias totam qui. Ipsa
+          cum repellat. Est suscipit tempora non. Maiores excepturi rerum iure qui impedit. Ut
+          suscipit vero voluptatem molestiae. Veritatis itaque numquam perspiciatis. Deleniti autem
+          nobis iste ut eaque et totam ipsum ut. Odio fugiat nihil consectetur eius provident.
+          Cupiditate dolore facilis omnis et vel quia deleniti et. Rem ea ullam optio est quam.
+          Beatae architecto consequatur laudantium et aut consequatur est eum quis. Dolore
+          voluptatem amet qui tenetur quide˝m. Sunt veniam maxime architecto animi sequi libero quia
+          architecto. Porro officia sequi quo. Qui commodi qui corporis molestias totam qui. Ipsa
+          cum repellat. Est suscipit tempora non. Maiores excepturi rerum iure qui impedit.
+        </span>
+        <Link href="/">
           <a>home</a>
         </Link>
       </main>
